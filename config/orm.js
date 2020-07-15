0db6350511a3614ff49a30f2f398eb59e5be334e
Scott Griffin <mscottgriffin@gmail.com>
2020-07-14 23:52:36 -0400

    added app.use express functions to server.js. Tested server listening

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+//connection orm
+const connection = require('./connection');
+//dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+
+module.exports = orm;

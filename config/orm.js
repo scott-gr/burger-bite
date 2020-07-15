fe4b271d629ec7db11ddce9844c52924dc141418
Scott Griffin <mscottgriffin@gmail.com>
2020-07-15 00:32:38 -0400

    added two functions to orm- question mark arrays for queries, msql syntax converter

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,40 @@
 //connection orm
 const connection = require('./connection');
-//dependencies
 
+///Loop creates array of question marks for queries. Credit to GT Bootcamp repo.
+function printQuestionMarks(num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+};
+
+// Helper function to convert object key/value pairs to SQL syntax Credit to GT Bootcamp repo.
+function objToSql(ob) {
+  let arr = [];
+  // loop through the keys and push the key/value as a string int arr
+  for (let key in ob) {
+    let value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  };
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+};
+
+const orm = {
+  selectAll: (table, cb) => {
+
+  }
+};
 
 
 module.exports = orm;

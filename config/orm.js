7460c10077ddfbd7c87ec605b21fdf973547e43d
Scott Griffin <mscottgriffin@gmail.com>
2020-07-15 00:19:38 -0400

    added routes to server.js, server.js and connection.js complete

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,7 @@
 //connection orm
 const connection = require('./connection');
 //dependencies
-const express = require("express");
-const exphbs = require("express-handlebars");
+
 
 
 module.exports = orm;

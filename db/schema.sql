492e9eefd64947ef0f6e32d5eda0611f67831df6
Scott Griffin <mscottgriffin@gmail.com>
2020-07-14 22:46:29 -0400

    schema and seeds working in mysql. Setting up connection

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers_db(
+CREATE TABLE burgers(
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(50) NOT NULL,
   devoured BOOLEAN DEFAULT false,

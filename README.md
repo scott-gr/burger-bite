81ae02cc56484d84ee4ebb81aa02b172cd469ea6
Scott Griffin <mscottgriffin@gmail.com>
2020-07-20 13:24:42 -0400

    styling index

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # burger-bite
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Demo](#demo)
+* [Contributors](#contributors)
+* [License](#license)
+* [Questions](#questions)
+
+## Description
+A fun application to log burgers that you'd like to eat and click a big button to eat them! Utilizes handlebars, sql, and nodejs.
+
+## Installation
+
+
+## Usage
+
+
+## Contributors
+I, Scott Griffin, am the sole programmer on this project. Idea and examples from Trilogy and GT Bootcamp. 
+
+## License
+MIT License
+
+## Questions
+Thank you for checking out my project. Please contact me at mscottgriffin@gmail.com or visit http://github.com/scott-gr
--- public/assets/js/buttonscripts.js
+++ public/assets/js/buttonscripts.js
@@ -1,2 +1,30 @@
-$(() => {
-  $("#submitBurger").on("click", event => {
+$(function () {
+  $('.devourBtn').on('click', function (event) {
+    var id = $(this).data('id');
+    var newDevoured = $(this).data('devoured');
+
+    var newEatenState = {
+      devoured: newDevoured,
+    };
+    $.ajax('/api/burgers/' + id, {
+      type: 'PUT',
+      data: newEatenState,
+    }).then(() => {
+      location.reload();
+    });
+  });
+
+  $('#submitBurger').on('submit', (event) => {
+    event.preventDefault();
+    var newBurger = {
+      name: $('#addBurger').val().trim(),
+      devoured: $("[name=devoured]").val().trim()
+    };
+    $.ajax('/api/burgers', {
+      type: 'POST',
+      data: newBurger,
+    }).then(() => {
+      location.reload();
+    });
+  });
+});
